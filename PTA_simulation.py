fix weights calculation and make it work for older numpy versions without append in diff

PTA_simulation.py
@@ -202,59 +202,48 @@ class PTA_sim:
 if __name__ == '__main__':
     rd.seed(1234)
     
-#    print('An example of PTA sim')
+    print('An example of PTA sim')
 #    # make a simulation object (we may want to have initialisation options that
-#    # automatically do the next few steps, but for now we do them by hand)
-#    Npsr = 5
-#    sim = PTA_sim()
-#
-#    # make some pulsars, in this case 5 random ones with some variation in rms
-#    # and plot a skymap (bigger markers are better pulsars)
-#    sim.random_pulsars(Npsr, sig_rms=5e-8)
-#    sim.plot_pulsar_map()
-#
-#    # set some evenly sampled times (default options)
-#    #sim.evenly_sampled_times()
-#     
-#    # randomized times
-#    sim.randomized_times()
-#
-#    # generate some (very) unevenly sampled times
-##    mean_cadences = 10**np.random.normal(6, 0.5, Npsr) # lognormal
-##    t_starts = np.random.rand(Npsr) * 10 * YEAR
-##    exp_gap_spacings = 10**np.random.normal(0.5, 0.5, Npsr) * YEAR
-##
-##    sim.gappy_times(mean_cadences=mean_cadences, t_starts=t_starts,
-##                    exp_gap_spacings = exp_gap_spacings)
-#
-#    # inject a sinusoid signal
-#    # arguments are: phase, amplitude, polarization, cos(i), GW frequency (rd/s)
-#    from GW_models import sinusoid_TD
-#    GW_args = [0.1, 1e-12, np.pi/7, 0.3, 4e-8]
-#    source = (0.8 * np.pi, 1.3 * np.pi)
-#    sim.inject_signal(sinusoid_TD, source, *GW_args)
-#
-#    # inject white noise
-#    sim.white_noise()
-#
-#    # plot the residuals
-#    sim.plot_residuals()
-#
-#    # compute Fourier domain residuals
-#    sim.fourier_residuals()
-#
-#    # plot the Fourier domain residuals
-#    sim.plot_residuals_FD()
-    
+    # automatically do the next few steps, but for now we do them by hand)
+    Npsr = 5
     sim = PTA_sim()
-    sim.random_pulsars(5, mean_rms=1e-7, sig_rms=3e-8)
-    sim.evenly_sampled_times()
-    
+
+    # make some pulsars, in this case 5 random ones with some variation in rms
+    # and plot a skymap (bigger markers are better pulsars)
+    sim.random_pulsars(Npsr, sig_rms=5e-8)
+    sim.plot_pulsar_map()
+
+    # set some evenly sampled times (default options)
+    #sim.evenly_sampled_times()
+     
+    # randomized times
+    sim.randomized_times()
+
+    # generate some (very) unevenly sampled times
+#    mean_cadences = 10**np.random.normal(6, 0.5, Npsr) # lognormal
+#    t_starts = np.random.rand(Npsr) * 10 * YEAR
+#    exp_gap_spacings = 10**np.random.normal(0.5, 0.5, Npsr) * YEAR
+#
+#    sim.gappy_times(mean_cadences=mean_cadences, t_starts=t_starts,
+#                    exp_gap_spacings = exp_gap_spacings)
+
+    # inject a sinusoid signal
+    # arguments are: phase, amplitude, polarization, cos(i), GW frequency (rd/s)
     from GW_models import sinusoid_TD
-    sinusoid_args = [0.1, 1e-12, np.pi/7, 0.3, 4e-8]
-    source = (0.8*np.pi, 1.3*np.pi)
-    sim.inject_signal(sinusoid_TD, source, *sinusoid_args)
-    
+    GW_args = [0.1, 1e-12, np.pi/7, 0.3, 4e-8]
+    source = (0.8 * np.pi, 1.3 * np.pi)
+    sim.inject_signal(sinusoid_TD, source, *GW_args)
+
+    # inject white noise
+    sim.white_noise()
+
+    # plot the residuals
+    sim.plot_residuals()
+
+    # compute Fourier domain residuals
     sim.fourier_residuals()
+
+    # plot the Fourier domain residuals
+    sim.plot_residuals_FD()
     
     

_PTA_sim_fourier.py
@@ -15,10 +15,12 @@ import matplotlib.pyplot as plt
 # we can make different functions like these to get different frequency spacings etc
 
 def _weights_matrix(self, times, freqs):
-    # append ensures we get n diff values if there are n times (instead of n-1)
-    weights = np.diff(times, append=times[-1])
-    # FIXME what should the last weight be??? 
-    weights[-1] = times[-1] - np.sum(weights)
+    # we want the first weight to be t1 - t0, the last to be tn - t(n-1), and 
+    # all the rest to be 1/2 (t(i+1) - t(i)) + 1/2 (t(i) - t(i-1))
+    middle_weights = 0.5 * np.diff(times)[:-1] + 0.5 * np.diff(times)[1:]
+    first_weight = np.array([times[1] - times[0]])
+    last_weight = np.array([times[-1] - times[-2]])
+    weights = np.concatenate((first_weight, middle_weights, last_weight))
         
     #matrix m_ij of weights(t_i) * exp(-2*pi*i*f_j*t_i)
     expanded_freqs = np.expand_dims(self._freqs, axis=-1)
@@ -37,8 +39,7 @@ def _setup_TOAs_fourier(self, fmax=1e-7, alpha=1):
     """
     # set minimum frequency based on longest time span between any TOAs
     # with alpha some integer constant of order 1 or experiment with higher alpha
-    all_times_not_nan = self._times[np.isfinite(self._times)]
-    Tmax = np.max(all_times_not_nan) - np.min(all_times_not_nan)
+    Tmax = np.nanmax(self._times) - np.nanmin(self._times)
     fmin = 1 / (alpha * Tmax)
     # fmax chosen based on astrophysical prior, step same as fmin
     self._freqs = np.arange(fmin, fmax+fmin, step=fmin)
@@ -73,9 +74,8 @@ def _setup_model_fourier(self):
     # The maximum frequency gives us a cadence
     step = 1 / (2 * np.max(self._freqs))
     # start before the first TOA and end after the last
-    all_times_not_nan = self._times[np.isfinite(self._times)]
-    t_min = np.min(all_times_not_nan) - step
-    t_max = np.max(all_times_not_nan) + step
+    t_min = np.nanmin(self._times) - step
+    t_max = np.nanmax(self._times) + step
     self._model_times = np.arange(t_min, t_max, step=step)
     
 #    ## TEST same model times as evenly sampled TOA times