d036d652023e9268768cb624c7f9c1adf926e13a
elinore <elinore.roebber@gmail.com>
2019-09-10 18:30:28 +0100

    added more (failing) fourier matrix tests

--- tests/test_funky_fourier.py
+++ tests/test_funky_fourier.py
@@ -7,7 +7,7 @@ import numpy.testing as npt
 import healpy as hp
 
 from ptacake import PTA_sim, YEAR, SkyMap
-from ptacake.matrix_fourier import fmat, flatten
+from ptacake.matrix_fourier import fmat, ifmat, flatten
 #from tests.setup_sim import setup_evenly_sampled
 
 
@@ -18,10 +18,8 @@ class Test_funky_fourier(unittest.TestCase):
         Test whether the funky fourier transform reduces to the FFT for evenly sampled
         data, and the same choice of frequencies as the FFT.
         """
-        print('running Test_funky_fourier...')
-
         sim = setup_evenly_sampled(seed=1234567, default_signal=True)
-        sim.plot_residuals()
+        #sim.plot_residuals()
 
         Dt = sim._times[0][1] - sim._times[0][0]
 
@@ -49,8 +47,6 @@ class Test_funky_fourier(unittest.TestCase):
         matrix_fourier and the SkyMap class. Compares to numpy (i)fft routines
         and checks that a round trip returns the original results
         """
-
-        print('running test_matrix_fourier_transformations')
 
         Npsr = 20
 
@@ -108,7 +104,77 @@ class Test_funky_fourier(unittest.TestCase):
         # fres seems to only have real components, but sky_fres is evenly split
         npt.assert_allclose(fres[:, -1], sky_fres[:, -1], atol=atol,
                             err_msg='Round-trip MFT disagrees with injection '
-                            'for the last frequency bin')
+                            'in the last frequency bin')
+
+
+    def test_even_fmat_ifmat(self):
+        # check that ifmat is the inverse of fmat for evenly sampled times
+
+        sim = PTA_sim()
+        sim.random_pulsars(3)
+        sim.evenly_sampled_times(cadence=2e6, T=2e8)
+        freqs = np.arange(5e-9, 1e-6 + 1e-9, 5e-9)  # should exactly match times
+
+        F = fmat(sim._times, freqs)
+        iF = ifmat(freqs, sim._times)
+
+        iFF = iF @ F
+        FiF = F @ iF
+
+        # check if proportional to the identity with diag, off-diag components
+        # stolen from https://stackoverflow.com/questions/43884189/check-if-a-large-matrix-is-diagonal-matrix-in-python
+        i, j = iFF.shape
+        iFF_off_diag = iFF.reshape(-1)[:-1].reshape(i - 1, j + 1)[:, 1:]
+        iFF_diag = np.diag(iFF)
+
+        i, j = FiF.shape
+        FiF_off_diag = FiF.reshape(-1)[:-1].reshape(i - 1, j + 1)[:, 1:]
+        FiF_diag = np.diag(FiF)
+
+        iFF_norm = 4  # why???
+        npt.assert_allclose(iFF_diag, iFF_norm, err_msg='diag(iF @ F) is not '
+                            'proportional to the identity matrix: '
+                            'diagonal elements not all equal')
+        npt.assert_allclose(iFF_off_diag, 0, atol=iFF_norm*1e-10,
+                            err_msg='iF @ F has nonzero off-diagonal elements')
+
+        FiF_norm = 2  # 2 makes a little more sense but still?
+        npt.assert_allclose(FiF_diag, FiF_norm, err_msg='diag(F @ iF)is not '
+                            'proportional to the identity matrix: '
+                            'diagonal elements not all equal')
+        npt.assert_allclose(FiF_off_diag, 0, atol=FiF_norm*1e-10,
+                            err_msg='F @ iF has nonzero off-diagonal elements')
+
+
+    def test_uneven_fmat_ifmat(self):
+        # check that fmat, ifmat are inverses even for unevenly sampled times
+        # note: since they're not square matrices, only one combination works
+
+        sim = PTA_sim()
+        sim.random_pulsars(3)
+        sim.randomized_times(mean_cadence=1e6, t_end=2e8) # heavily oversampled
+        Ts = (sim._times[:, -1] - sim._times[:, 0])
+        T = np.mean(Ts)
+        freqs = np.arange(1/T, 1e-7, 1/T)
+
+        F = fmat(sim._times, freqs)
+        iF = ifmat(freqs, sim._times)
+
+        # only checking t -> f, because f -> t loses information
+        FiF = F @ iF
+
+        i, j = FiF.shape
+        off_diag = FiF.reshape(-1)[:-1].reshape(i - 1, j + 1)[:, 1:]
+        diag = np.diag(FiF)
+
+        norm = 2
+        atol = np.max(np.abs(1/Ts - 5e-9)/5e-9)  # this is pretty large
+        npt.assert_allclose(diag, norm, atol=atol*norm, err_msg='F @ iF is not '
+                            'proportional to the identity matrix: significant '
+                            'variation in diagonal elements')
+        npt.assert_allclose(off_diag, 0, atol=atol*norm, err_msg='F @ iF is '
+                            'not proportional to the identity matrix: '
+                            'large off-diagonal elements')
 
 
 
