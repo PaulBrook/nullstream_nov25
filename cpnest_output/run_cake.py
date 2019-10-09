006045db8b2f8ab292e5e87163cad93b56afa76e
Janna Goldstein <jgoldstein@star.bham.ac.uk>
2019-10-09 11:31:58 +0100

    update cpnest_run so it can resume from a previously made pta_sim (stored in a pickle file)

--- cpnest_output/run_cake.py
+++ cpnest_output/run_cake.py
@@ -9,10 +9,12 @@ import yaml
 import os
 from os.path import join, isfile
 import argparse
+import pickle
 
 import ptacake
 
 ### command line options for sim and run config ###
+
 parser = argparse.ArgumentParser("Make a PTA sim, then run cpnest")
 parser.add_argument('-s', '--sim_config', required=True, dest='sim_config',
                     help='config file for PTA simulation')
@@ -26,54 +28,15 @@ if not isfile(args.sim_config):
 if not isfile(args.run_config):
     parser.error('run config file {} does not exist!'.format(args.run_config))
     
-### read in sim config and make PTA sim ###
+    
+### read in sim config and run config ###
     
 with open(args.sim_config, 'r') as f1:
     sim_config = yaml.safe_load(f1)
-
-sim = ptacake.PTA_sim()
-
-# pulsar stuff
-if sim_config['pulsar_method'] == 'random':
-    pulsar_opts = sim_config['pulsar_opts']
-    num_pulsars = pulsar_opts.pop('num_pulsars')
-    sim.random_pulsars(num_pulsars, **sim_config['pulsar_opts'])
     
-elif sim_config['pulsar_method'] == 'from_file':
-    sim.pulsars_from_file(sim_config['pulsar_file'])
-    
-elif sim_config['pulsar_method'] == 'from_array':
-    sim.set_pulsars(sim_config['pulsar_array'], sim_config['pulsar_rms'])
-    
-
-# times stuff
-if sim_config['times_evenly_sampled']:
-    sim.evenly_sampled_times(**sim_config['times_es_opts'])
-else:
-    sim.randomized_times(**sim_config['times_rd_opts'])
-
-# signal and noise stuff
-if sim_config['model_name'] in ['sinusoid_TD', 'Sinusoid_TD']:
-    from ptacake.GW_models import sinusoid_TD
-    sim.inject_signal(sinusoid_TD, sim_config['true_source'], *sim_config['true_args'])
-else:
-    raise NotImplementedError('Model {} not yet implemented'.format(sim_config['model_name']))
-
-if sim_config['white_noise']:
-    sim.white_noise()
-    
-
-### read run config, prepare for run ###
-
 with open(args.run_config, 'r') as f2:
     run_config = yaml.safe_load(f2)
     
-# if using FD likelihood, need to run fourier_residuals
-# if using FD_ns likelihood, need to run concatenate_residuals also
-if 'FD' in run_config['ll_name']:
-    sim.fourier_residuals()
-if run_config['ll_name'] == 'FD_ns':
-    sim.concatenate_residuals()
     
 ### get and adjust output path ###
     
@@ -82,7 +45,7 @@ outdir = run_config['output_path']
 if 'TMPDIR' in os.environ:
     tmpdir = os.environ['TMPDIR']
     print('Found TMPDIR (putting output dir within) {}'.format(tmpdir))
-    outdir = os.path.join(tmpdir, outdir)
+    outdir = join(tmpdir, outdir)
     
 # normpath removes excess '/./'
 outdir = os.path.normpath(outdir)
@@ -90,7 +53,63 @@ print('Putting plot etc output in {}'.format(outdir))
 
 if not os.path.exists(outdir):
     os.mkdir(outdir)
-    print('created dir {}. succes? {}'.format(outdir, os.path.exists(outdir)))
+    print('created dir {}. succes? {}'.format(outdir, os.path.exists(outdir)))    
+
+
+### make or load PTA sim ###
+    
+# first check for pta sim pickle (resume from previous run)  
+sim_pickle_path = join(outdir, 'pta_sim.pickle')
+try:
+    with open(sim_pickle_path, 'rb') as f:
+        sim = pickle.load(f)
+        print('resuming run with stored pta_sim in {}'.format(sim_pickle_path))
+        
+except FileNotFoundError:
+    # we do not have a sim pickle to resume from a previous run, 
+    # so do the simulation
+    sim = ptacake.PTA_sim()
+    
+    # pulsar stuff
+    if sim_config['pulsar_method'] == 'random':
+        pulsar_opts = sim_config['pulsar_opts']
+        num_pulsars = pulsar_opts.pop('num_pulsars')
+        sim.random_pulsars(num_pulsars, **sim_config['pulsar_opts'])
+        
+    elif sim_config['pulsar_method'] == 'from_file':
+        sim.pulsars_from_file(sim_config['pulsar_file'])
+        
+    elif sim_config['pulsar_method'] == 'from_array':
+        sim.set_pulsars(sim_config['pulsar_array'], sim_config['pulsar_rms'])
+        
+    # times stuff
+    if sim_config['times_evenly_sampled']:
+        sim.evenly_sampled_times(**sim_config['times_es_opts'])
+    else:
+        sim.randomized_times(**sim_config['times_rd_opts'])
+    
+    # signal and noise stuff
+    if sim_config['model_name'] in ['sinusoid_TD', 'Sinusoid_TD']:
+        from ptacake.GW_models import sinusoid_TD
+        sim.inject_signal(sinusoid_TD, sim_config['true_source'], *sim_config['true_args'])
+    else:
+        raise NotImplementedError('Model {} not yet implemented'.format(sim_config['model_name']))
+    
+    if sim_config['white_noise']:
+        sim.white_noise()
+        
+        
+    # if using FD likelihood, need to run fourier_residuals
+    # if using FD_ns likelihood, need to run concatenate_residuals also
+    if 'FD' in run_config['ll_name']:
+        sim.fourier_residuals()
+    if run_config['ll_name'] == 'FD_ns':
+        sim.concatenate_residuals()
+        
+    # save the sim as a pickle in case we resume later
+    with open(sim_pickle_path, 'wb') as f:
+        pickle.dump(sim, f)
+        
     
 ### optional plotting/save S/N ###
     
@@ -110,6 +129,7 @@ if sim_config['plot_residuals_FD'] and 'FD' in run_config['ll_name']:
     fig2, ax2 = sim.plot_residuals_FD()
     fig2.savefig(join(outdir, 'FDresiduals.pdf'))
     
+    
 ### select sampler and run! ###
     
 if run_config['sampler'] == 'cpnest':
@@ -121,7 +141,8 @@ elif run_config['sampler'] == 'grid':
 else:
     raise ValueError('Unknown sampler {}'.format(run_config['sampler']))
 
-# pass outdir
+print('Moving to run... \n')
+# call sampler run with sim object, run_config and output directory
 run(sim, run_config, outdir=outdir)
 
     
