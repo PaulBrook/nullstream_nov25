fix minus sign error in norm of FD null-stream likelihood

ptacake/_PTA_sim_likelihood.py
@@ -104,10 +104,8 @@ def log_likelihood_TD(self, source, model_func, model_args,
         
         # use log(det(cov)) = -log(det(inv_cov))
         sign, logdet_inv_cov = np.linalg.slogdet(inv_cov)
-        logdet_cov = - logdet_inv_cov
-        #print('log( det( cov)): {}'.format(logdet_cov))
-        norm += -num_times*hl2p - 0.5*logdet_cov
-    
+        norm += -num_times*hl2p + 0.5*logdet_inv_cov
+        
     if return_only_norm:
         return norm
     

ptacake/_PTA_sim_nullstream.py
@@ -109,7 +109,7 @@ def log_likelihood_FD_ns(self, source, model_func, model_args,
     # no 0.5 in norm because complex quantity
     # for norm, use the inv_cov WITHOUT null-stream transformation
     sign, log_det_inv_cov = la.slogdet(self._inv_big_FD_cov)
-    norm = N*P*np.log(2*np.pi) - log_det_inv_cov
+    norm = - N*P*np.log(2*np.pi) + log_det_inv_cov
     
     assert(abs(np.imag(ll)) < abs(np.real(ll) * 1e-10))
     ll = np.real(ll)